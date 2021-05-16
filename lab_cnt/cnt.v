d054e67d411c6255e78ea74e984db74c04e135c2
wellytung <wellytung1@gmail.com>
2021-05-17 00:20:36 +0800

    modified:   cnt.v 	update for lab_cnt_1

--- lab_cnt/cnt.v
+++ lab_cnt/cnt.v
@@ -1,23 +1,48 @@
  module cnt(
    clk,
-   rst_n
+   rst_n,
+   cnt_hit
  );
  
  input  clk;
  input  rst_n;
+ output cnt_hit;
  
  reg [9:0] cnt;
  reg [9:0] cnt_nxt;
+
+ reg [2:0] cnt_5;
+ reg [2:0] cnt_5_nxt;
+
+ reg cnt_hit;
  
  // sequential logic 
  always@(posedge clk or negedge rst_n)
    if (~rst_n) cnt <= 10'd0;
    else        cnt <= cnt_nxt;
+
+ always@(posedge clk or negedge rst_n)
+   if (~rst_n) cnt_5 <= 10'd0;
+   else        cnt_5 <= cnt_5_nxt;
      
  
  // combinational logic
  always@(*) begin
    cnt_nxt = cnt + 1;
+ end
+
+ always@(*) begin
+   if (cnt_5 == 4)
+     cnt_5_nxt = 3'd0;
+   else
+     cnt_5_nxt = cnt_5 + 1;
+ end
+
+ always@(*) begin
+   if (cnt_5 == 0)
+     cnt_hit = 1'b1;
+   else
+     cnt_hit = 1'b0;
  end
    
  endmodule
